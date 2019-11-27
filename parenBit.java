e65806bdcdc1290c083b5f527d17ff5a8dce63da
Skanda Bhargav <5369621+SkandaB@users.noreply.github.com>
2019-11-26 19:22:01 -0800

    Update parenBit.java

--- parenBit.java
+++ parenBit.java
@@ -35,3 +35,24 @@ public String parenBit(String str) {
         // then chop off both, first and last character, and repeat
         return parenBit(str.substring(1, str.length() - 1));
     }
+
+
+/*
+Solution from quetion website
+public String parenBit(String str) {
+  if (str.charAt(0) != '(') {
+    return parenBit(str.substring(1));
+  }
+  if (str.charAt(str.length()-1) != ')') {
+    return parenBit(str.substring(0, str.length()-1));
+  }
+  return str;
+  
+  // Solution notes: this is tricky. Is the first char a '('?
+  // If not, recur, removing one char from the left of the string.
+  // Eventually this gets us to '(' at the start of the string.
+  // If the first char is '(', then recur similarly, removing one char from
+  // the end of the string, until it is ')'.
+  // Now the first and last chars are ( .. ) and you're done.
+}
+*/
